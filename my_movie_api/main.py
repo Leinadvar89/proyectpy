def277150c573b99fc35ea77d378895ee9266088
Leinadvar89 <sfxtecnologia@gmail.com>
2023-07-06 04:34:40 -0500

    FASTAPI

--- my_movie_api/main.py
+++ my_movie_api/main.py
@@ -1,93 +1,88 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
-app.title = "Mi aplicacion con FastAPI"
+app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(le=2022)
+    rating:float = Field(ge=1, le=10)
+    category:str = Field(min_length=5, max_length=15)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "Mi película",
+                "overview": "Descripción de la película",
+                "year": 2022,
+                "rating": 9.8,
+                "category" : "Acción"
+            }
+        }
 
 movies = [
     {
-
-        "id": 1,
-        "title": "Avatar",
-        "overview": "Lo mismo que pocahontas pero con extraterrestres",
-        "year": "2009",
-        "rating": 7.8,
-        "category": "aventura"
-    },
-        {
-
-        "id": 2,
-        "title": "interestelar",
-        "overview": "Lo mismo que pocahontas pero con extraterrestres",
-        "year": "2010",
-        "rating": 9.9,
-        "category": "Ficcion"
-    },
-        {
-
-        "id": 3,
-        "title": "Arrival",
-        "overview": "Lo mismo que pocahontas pero con extraterrestres",
-        "year": "2011",
-        "rating": 9.8,
-        "category": "Ficcion"
-    },
-        {
-
-        "id": 4,
-        "title": "La chica del dragon tatuado",
-        "overview": "Lo mismo que pocahontas pero con extraterrestres",
-        "year": "2012",
-        "rating": 9.7,
-        "category": "Drama"
-    }
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
 ]
 
 @app.get('/', tags=['home'])
 def message():
-    return HTMLResponse('<h1>Hello world =) </h1>')
+    return HTMLResponse('<h1>Hello world</h1>')
 
 @app.get('/movies', tags=['movies'])
 def get_movies():
     return movies
 
 @app.get('/movies/{id}', tags=['movies'])
-def get_movies(id:int):
+def get_movie(id: int = Path(ge=1, le=2000)):
     for item in movies:
         if item["id"] == id:
             return item
     return []
 
 @app.get('/movies/', tags=['movies'])
-def get_movies_by_category(category: str, year: int):
-    return [ item for item in movies if item['category'] == category]
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)):
+    return [ item for item in movies if item['category'] == category ]
 
 @app.post('/movies', tags=['movies'])
-def create_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
-    movies.append({
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category,
-    })
+def create_movie(movie: Movie):
+    movies.append(movie)
     return movies
 
-@app.put('/movies/{id}',tags=['movies'])
-def update_movies(id: int, title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
-    for item in movies:
-        if item["id"]== id:
-            item['title'] = title,
-            item['overview'] = overview,
-            item['year'] = year,
-            item['rating'] = rating,
-            item['category'] = category,
-            return movies
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id: int, movie: Movie):
+	for item in movies:
+		if item["id"] == id:
+			item['title'] = movie.title
+			item['overview'] = movie.overview
+			item['year'] = movie.year
+			item['rating'] = movie.rating
+			item['category'] = movie.category
+			return movies
 
-@app.delete('/movies/{id}',tags=['movies'])
-def delete_movie(id:int):
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
     for item in movies:
         if item["id"] == id:
             movies.remove(item)
